c502b72728c08e5a986108abac52857032ff6060
ziadlinz <44933743+ziadlinz@users.noreply.github.com>
2018-11-15 01:34:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "510227184216965148"; // ايدي السررفر
+var channel = "511739393053032448";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('bem ba bra bem bom how')
     },305);
 })
 
